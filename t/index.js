52f5075bec74cd67b1d7eac1c6685cabb19e86fa
Nickolay Platonov <nickolay@desktop.(none)>
2010-08-20 18:17:17 +0400

    - switched to Dist::Zilla

---
+++ t/index.js
@@ -0,0 +1,38 @@
+var Harness
+var isNode        = typeof process != 'undefined' && process.pid
+
+if (isNode) {
+    require('Task/Test/Run/NodeJSBundle')
+    
+    Harness = Test.Run.Harness.NodeJS
+} else 
+    Harness = Test.Run.Harness.Browser.ExtJS
+        
+    
+var INC = (isNode ? require.paths : []).concat('lib', '../lib', '/jsan')
+
+
+
+Harness.configure({
+	title : 'Symbie test suite',
+	
+	passThroughEx : true,
+	
+	preload : [
+        'Task.Symbie.Bundle',
+        {
+            text : "JooseX.Namespace.Depended.Manager.my.INC = " + Ext.encode(Harness.absolutizeINC(INC))
+        }
+    ]
+})
+
+
+Harness.start(
+	'010_route_meta.t.js',
+	'020_route_matching_and_stringification.t.js',
+	'030_router_dispatching.t.js',
+	'040_widget_id.t.js',
+	'050_application_basics.t.js',
+	'051_application_dispatch.t.js',
+	'052_application_activation_tree_marks.t.js'
+)
